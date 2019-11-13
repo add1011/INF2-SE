d2c759573bc286ec2c6b5e043d595dbe82db9392
add1011 <adelsefiane@gmail.com>
2019-11-13 16:30:41 +0000

    Added to DiscountPolicy

--- src/uk/ac/ed/bikerental/DiscountPolicy.java
+++ src/uk/ac/ed/bikerental/DiscountPolicy.java
@@ -2,8 +2,16 @@ package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscountPolicy implements PricingPolicy {
+    private Map<BikeType, BigDecimal> dailyRentalPrice;
+
+    public DiscountPolicy() {
+        this.dailyRentalPrice = new HashMap<>();
+    }
+
     @Override
     public void setDailyRentalPrice(BikeType bikeType, BigDecimal dailyPrice) {
 
@@ -11,6 +19,10 @@ public class DiscountPolicy implements PricingPolicy {
 
     @Override
     public BigDecimal calculatePrice(Collection<Bike> bikes, DateRange duration) {
+        BigDecimal totalPrice = new BigDecimal("0");
+        for (Bike bike : bikes) {
+
+        }
 
         return null;
     }
