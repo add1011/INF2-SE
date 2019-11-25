4eb95001bcefb160f80af16833c8049aad7d300c
adel <s1839825@ed.ac.uk>
2019-11-25 16:07:00 +0000

    Actually made test for discount policy

--- tests/uk/ac/ed/bikerental/TestDiscountPolicy.java
+++ tests/uk/ac/ed/bikerental/TestDiscountPolicy.java
@@ -9,25 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
-
-
-
-
 
 @DisplayName("Testing Discount Policy Class")
 class TestDiscountPolicy {
     private DiscountPolicy discount1 = new DiscountPolicy();
     //private DiscountPolicy discount2 = new DiscountPolicy();
-
-    private BigDecimal totalDailyPrice = new BigDecimal(0);
     private Collection<Bike> bikes = new ArrayList<>();
-
-
-
-
 
     @DisplayName("Sets up daily rental prices for each DiscountPolicy object")
     @BeforeEach
@@ -68,19 +59,16 @@ class TestDiscountPolicy {
                 bikeType4
         );
 
-
         bikes.add(bike1);
         bikes.add(bike2);
         bikes.add(bike3);
         bikes.add(bike4);
-
 
         //Setting up custom daily rental prices for each DiscountPolicy object
         BigDecimal dailyPrice1= new BigDecimal(40.0);
         BigDecimal dailyPrice2= new BigDecimal(3);
         BigDecimal dailyPrice3= new BigDecimal(15);
         BigDecimal dailyPrice4= new BigDecimal(22);
-
 
         discount1.setDailyRentalPrice(bikeType1, dailyPrice1);
         discount1.setDailyRentalPrice(bikeType2, dailyPrice2);
@@ -90,33 +78,24 @@ class TestDiscountPolicy {
         //discount2.setDailyRentalPrice(bikeType2, dailyPrice2);
         //discount2.setDailyRentalPrice(bikeType3, dailyPrice3);
         //discount2.setDailyRentalPrice(bikeType4, dailyPrice4);
-
-
-
-        totalDailyPrice = totalDailyPrice.add(dailyPrice1);
-        totalDailyPrice = totalDailyPrice.add(dailyPrice2);
-        totalDailyPrice = totalDailyPrice.add(dailyPrice3);
-        //totalDailyPrice = totalDailyPrice.add(dailyPrice4);
-
-
-
     }
 
     @Test
     void testCalculatePrice(){
-        DateRange duration1 = new DateRange(LocalDate.of(2019,3,3),
-                LocalDate.of(2019,3,2  )); //1-2 days
+        DateRange duration1 = new DateRange(LocalDate.of(2019,3,2),
+                LocalDate.of(2019,3,3  )); //1-2 days
 
         DateRange duration2 = new DateRange(LocalDate.of(2019,3,3),
                 LocalDate.of(2019,3,6  )); //3-6 days
 
         DateRange duration3 = new DateRange(LocalDate.of(2019,3,3),
-                LocalDate.of(2019,3,4  )); //7-13 days
+                LocalDate.of(2019,3,11  )); //7-13 days
 
         DateRange duration4 = new DateRange(LocalDate.of(2019,3,3),
                 LocalDate.of(2019,5,4  )); // 14+ days
-        DateRange duration5 = new DateRange(LocalDate.of(2019,3,3),
-                LocalDate.of(2019,3,3  )); //0 days
+
+        DateRange duration5 = new DateRange(LocalDate.of(2019,12,30),
+                LocalDate.of(2020,1,3  )); //dateRange wraps round year
 
 
         //System.out.println(totalPrice1);
@@ -127,17 +106,29 @@ class TestDiscountPolicy {
         //totalPrice2 =  totalPrice2.multiply(discount2.calculatePrice(bikes, duration4));
 
         BigDecimal expectedPrice1 = new BigDecimal((40.0+3+15)*1);
+        expectedPrice1 = expectedPrice1.setScale(2, RoundingMode.HALF_UP);
         BigDecimal actualPrice1 = discount1.calculatePrice(bikes, duration1);
 
-        assertEquals(expectedPrice1, actualPrice1);
-        //assertEquals(totalPrice1,totalDailyPrice.multiply(new BigDecimal(0.95) ) );
-        /**assertEquals(totalPrice1,totalDailyPrice.multiply(new BigDecimal(0.90) ) );
-        assertEquals(totalPrice1,totalDailyPrice.multiply(new BigDecimal(0.85) ) );
+        BigDecimal expectedPrice2 = new BigDecimal((40.0+3+15)*0.95);
+        expectedPrice2 = expectedPrice2.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal actualPrice2 = discount1.calculatePrice(bikes, duration2);
 
-        assertEquals(totalPrice2,totalDailyPrice.multiply(new BigDecimal(1) ) );
-        assertEquals(totalPrice2,totalDailyPrice.multiply(new BigDecimal(0.95) ) );
-        assertEquals(totalPrice2,totalDailyPrice.multiply(new BigDecimal(0.90) ) );
-        assertEquals(totalPrice2,totalDailyPrice.multiply(new BigDecimal(0.85) ) );
-         **/
+        BigDecimal expectedPrice3 = new BigDecimal((40.0+3+15)*0.9);
+        expectedPrice3 = expectedPrice3.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal actualPrice3 = discount1.calculatePrice(bikes, duration3);
+
+        BigDecimal expectedPrice4 = new BigDecimal((40.0+3+15)*0.85);
+        expectedPrice4 = expectedPrice4.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal actualPrice4 = discount1.calculatePrice(bikes, duration4);
+
+        BigDecimal expectedPrice5 = new BigDecimal((40.0+3+15)*0.95);
+        expectedPrice5 = expectedPrice5.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal actualPrice5 = discount1.calculatePrice(bikes, duration5);
+
+        assertEquals(expectedPrice1, actualPrice1);
+        assertEquals(expectedPrice2, actualPrice2);
+        assertEquals(expectedPrice3, actualPrice3);
+        assertEquals(expectedPrice4, actualPrice4);
+        assertEquals(expectedPrice5, actualPrice5);
     }
 }
