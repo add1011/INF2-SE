b0e94bb0bf0617a57213dd5cdc29bee0ba0128dd
wassim2u <wassim.jabrane@gmail.com>
2019-11-28 22:18:55 +0000

    Added comments to what collectionMethod does

--- src/uk/ac/ed/bikerental/collectionMethod.java
+++ src/uk/ac/ed/bikerental/collectionMethod.java
@@ -1,5 +1,9 @@
 package uk.ac.ed.bikerental;
 
+//collectionMethod denotes the how the customer wishes to get the
+//bikes, whether by picking them up by themselves or
+// by using the DeliveryService functionality and have a driver send it to
+//them
 public enum collectionMethod {
     PickUp, Delivery
 }
